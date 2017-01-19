67ed796165ddc4fa3d88e8c308976f09be8fc6cb
jarupong pajakgo <lagmanmc@gmail.com>
2017-01-19 20:59:51 +0700

    make function for merge data to array

--- backend/util/array.php
+++ backend/util/array.php
@@ -1,5 +1,6 @@
 <?php
 
+declare(strict_types=1);
 class array_tool
 {
     public function __construct()
@@ -8,6 +9,14 @@ class array_tool
     public function To_Onedimen(array $array)
     {
         $result = array_reduce($array, 'array_merge', array());
-        return (array)$result;
+
+        return (array) $result;
+    }
+    public function merge()
+    {
+        $return = array();
+        $param = func_get_args();
+
+        return (array) $return;
     }
 }
