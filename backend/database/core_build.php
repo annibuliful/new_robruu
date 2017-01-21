4843c89b568d51ac8c249d71e8719f59f1919708
jarupong pajakgo <lagmanmc@gmail.com>
2017-01-21 14:05:58 +0700

    make function for use function from SelectQuery class

--- backend/database/core_build.php
+++ backend/database/core_build.php
@@ -1,16 +1,38 @@
 <?php
 declare(strict_types=1);
-require dirname(__DIR__).'\config\DB.php';
 require dirname(__DIR__).'\database\SelectQuery.php';
-class core_build extends DB_config
+require dirname(__DIR__).'\database\JoinQuery.php';
+class core_build
 {
-  private $pdo;
-  private $select;
+  // @var $pdo เก็บค่า PDO
+  public $pdo;
+
+  // @var $select instance สำหรับ SelectQuery class
+  public $select;
+
+  // @var $join instance สำหรับ JoinQuery class
+  public $join;
+
+  // @var $param เก็บ param เอาไว้ทำ bind parameter
+  public $param;
+
+  // @var $sql เก็บ SQL command เพื่อ execute
+  public $sql;
+
   function __construct()
   {
-    $this->pdo = new PDO($this->dsn,$this->user,$this->password);
-    $this->select = new SelectQuery();
+    $this->join = new JoinQuery();
+  }
+
+  /*
+  * ฟังก์ชั่นสำหรับในการ select data แล้ว fetch
+  * @return $this
+  */
+  public function select(string $table, array $columns = null)
+  {
+    $select = new SelectQuery();
+    $select->select($table,$columns);
+    return $select;
   }
 }
-
  ?>
