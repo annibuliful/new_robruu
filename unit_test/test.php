3549279aa6adf8493aa483d778d55c0a691e7ee3
jarupong pajakgo <lagmanmc@gmail.com>
2017-02-05 22:10:47 +0700

    fix test file

--- unit_test/test.php
+++ unit_test/test.php
@@ -5,19 +5,22 @@ class DataTest extends TestCase
 
  /**
  * @dataProvider select
+ * @backupGlobals disabled
+ * @backupStaticAttributes disabled
  */
  public function testselect($table, $columns, $result)
  {
-   $sql = new SelectQuery();
+   $pdo = new PDO('mysql:dbname=test;host=127.0.0.1','root','@PeNtesterMYSQL');
+   $sql = new SelectQuery($pdo);
  $this->assertEquals($result, $sql->select($table,$columns)->getSql());
  }
 
  public function select()
  {
  return [
- ['test',array(),'SELECT * FROM test'],
- ['test', array('test1','test2'),'SELECT test1,test2 FROM test'],
- ['test',array('test1','test2','test3','test4'),'SELECT test1,test2,test3,test4 FROM test']
+ ['test',array(),'SELECT test.* FROM test '],
+ ['test', array('test1','test2'),'SELECT test.test1,test.test2 FROM test '],
+ ['test',array('test1','test2','test3','test4'),'SELECT test.test1,test.test2,test.test3,test.test4 FROM test ']
  ];
  }
 }
